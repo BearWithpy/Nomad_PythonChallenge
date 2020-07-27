a712ec82613569b1c5c7ed053bb89b23bc428f0d
박준수 <pksjmh5295@gmail.com>
2020-07-27 13:29:00 +0900

    upload example file

--- final/final.py
+++ final/final.py
@@ -12,6 +12,8 @@ from flask import (
     render_template,
     request,
     send_file,
+    redirect,
+    url_for,
     Response,
 )
 from io import StringIO
@@ -44,22 +46,73 @@ def search():
     return render_template("result.html", jobs=jobs, term=term)
 
 
+#
+# @app.route("/export")
+# def export():
+#     term = request.args.get("term").lower()
+#     output = StringIO()
+#     output.write("Link,Title,Company\n")
+#
+#     for job in db[term]:
+#         for idx, val in enumerate(job.values()):
+#             output.write(str(val))
+#
+#             if idx < (len(job) - 1):
+#                 output.write(",")
+#
+#         output.write("\n")
+#
+#     response = Response(
+#         output.getvalue(), mimetype="text/csv", content_type="application/octet-stream",
+#     )
+#     response.headers["Content-Disposition"] = f"attachment; filename={term}.csv"
+#
+#     return response
+#
+#     # https://frhyme.github.io/python-libs/file_download_with_flask/
+#
+#     # term = request.args.get("term").lower()
+#     # with open(f"{term}.csv", mode="w", encoding="utf-8-sig") as job_file:
+#     #     writer = csv.writer(job_file)
+#     #     writer.writerow(["Link", "Company", "Title"])
+#     #     for job in db[term]:
+#     #         writer.writerow(list(job.values()))
+#     #
+#     # return send_file(
+#     #     job_file,
+#     #     mimetype="text/csv",
+#     #     as_attachment=True,
+#     #     attachment_filename=f"{term}.csv",
+#     # )
+
+
 @app.route("/export")
 def export():
-    # https://frhyme.github.io/python-libs/file_download_with_flask/
     term = request.args.get("term").lower()
-    with open(f"{term}.csv", mode="w", encoding="utf-8-sig") as job_file:
-        writer = csv.writer(job_file)
-        writer.writerow(["Link", "Company", "Title"])
-        for job in db[term]:
-            writer.writerow(list(job.values()))
+    try:
+        output = StringIO()
+        output.write("Link,Title,Company\n")
 
-    return send_file(
-        job_file,
-        mimetype="text/csv",
-        as_attachment=True,
-        attachment_filename=f"{term}.csv",
-    )
+        for job in db[term]:
+            for i, value in enumerate(job.values()):
+                output.write(str(value))
+
+                if i < (len(job) - 1):
+                    output.write(",")
+
+            output.write("\n")
+
+        response = Response(
+            output.getvalue(),
+            mimetype="text/csv",
+            content_type="application/octet-stream",
+        )
+
+        response.headers["Content-Disposition"] = f"attachment; filename={term}.csv"
+
+        return response
+    except Exception:
+        return redirect(url_for("home"))
 
 
 app.run(host="0.0.0.0", debug=True, port=7000)
